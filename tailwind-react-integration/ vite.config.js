414517e4101b0446fb2dd6416beee1a37c3d4ad1
Ali Munir Abdullahi <alimunirabdullahi@gmail.com>
2025-11-30 09:33:30 +0300

    Update  vite.config.js

--- tailwind-react-integration/ vite.config.js
+++ tailwind-react-integration/ vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+    import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // ✅ Add this
 
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(),
+        react(),
+        tailwindcss(), // ✅ Add this
   ],
 })
