a8c49247cf933adf0596414168935454dce9d871
Ali Munir Abdullahi <alimunirabdullahi@gmail.com>
2025-12-11 16:11:23 +0300

    Create App.jsx

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import FormikForm from './components/formikForm'; // Ensure the path is correct
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <FormikForm />
+    </div>
+  );
+};
+
+export default App;
